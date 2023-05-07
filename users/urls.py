ae46d9d186a280132a3345e28e43906e20f27a28
Martin Kribs <54333035+martinkribs@users.noreply.github.com>
2023-05-07 18:30:49 +0200

    working login

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('login_user', views.loginUser, name="login"),
+    path('register_user', views.registerUser, name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def loginUser(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request,("There was an Error Logging In, Try Again ..."))
+            return redirect('login')
+    else:
+        return render(request,'authenticate/login.html', {})
+
+def registerUser(request):
+    return render(request,'authenticate/register.html', {})
